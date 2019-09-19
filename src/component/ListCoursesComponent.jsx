a74d655120ca61fe27a16b11cf39be0c6d4e9b6f
ddjaell <ljy2905.92@gmail.com>
2019-09-19 14:38:14 +0900

    first commit

---
+++ src/component/ListCoursesComponent.jsx
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import CourseDataService from '../service/CourseDataService'
+class ListCoursesComponent extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            courses: [],
+            message: null
+        }
+        this.refreshCourses = this.refreshCourses.bind(this)
+        this.deleteCourseClicked = this.deleteCourseClicked.bind(this)
+    }
+
+    componentDidMount() {
+        this.refreshCourses();
+    }
+
+    refreshCourses() {
+        CourseDataService.retrieveAllCourses('')//HARDCODED
+            .then(
+                response => {
+                    //console.log(response);
+                    this.setState({ courses: response.data })
+                }
+            )
+    }
+    deleteCourseClicked(id) {
+        CourseDataService.deleteCourse('', id)
+            .then(
+                response => {
+                    this.setState({ message: `Delete of course ${id} Successful` })
+                    this.refreshCourses()
+                }
+            )
+    
+    }
+    render() {
+        return (
+            <div className="container">
+                <h3>All Courses</h3>
+                {this.state.message && <div class="alert alert-success">{this.state.message}</div>}
+                <div className="container">
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th>Id</th>
+                                <th>Description</th>
+                                <th>Delete</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                this.state.courses.map(
+                                    course =>
+                                        <tr key={course.id}>
+                                            <td>{course.id}</td>
+                                            <td>{course.description}</td>
+                                            <td><button className="btn btn-warning" onClick={() => this.deleteCourseClicked(course.id)}>Delete</button></td>
+                                        </tr>
+                                )
+                            }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default ListCoursesComponent
